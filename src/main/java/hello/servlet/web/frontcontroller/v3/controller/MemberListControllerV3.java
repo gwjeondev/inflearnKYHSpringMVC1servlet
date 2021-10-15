2d11556b5fe02c57c48802a259a0a272b84537f4
won-developer <gw4609@gmail.com>
2021-10-15 21:55:48 +0900

    MVC 프레임워크 만들기: Model 추가 - v3

--- src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -2,9 +2,8 @@ package hello.servlet.web.frontcontroller.v3.controller;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.v3.ControllerV3;
-import org.springframework.ui.Model;
+import hello.servlet.web.frontcontroller.ModelView;
 
 import java.util.List;
 import java.util.Map;
@@ -15,11 +14,11 @@ public class MemberListControllerV3 implements ControllerV3 {
 
     @Override
     public ModelView process(Map<String, String> paramMap) {
+        ModelView mv = new ModelView("members");
         List<Member> members = memberRepository.findAll();
 
-        ModelView mv = new ModelView("members");
         mv.getModel().put("members", members);
-
         return mv;
     }
+
 }
